e15a409fb73ef53902dcd861e4ab3c386f8e9de6
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-25 13:00:42 -0800

    Added tests for user first name, last name and profile correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,18 @@ class User < ActiveRecord::Base
                           :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+  						    uniqueness: true,
+  						    format: {
+  						    	  # Regular expression that makes sure name is formatted correctly.
+  						    	  # Allows lower case letters, upper case letters, numbers, under-
+  						    	  # scores and dashes.
+  						    	  with: /a-zA-Z0-9_-/,
+  						    	  message: 'Must be formatted correctly.'
+                                      }
+
   # A user can have many statuses
   has_many :statuses
 
