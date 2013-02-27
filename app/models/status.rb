adda68c9628df10f274cba466d4df434901b1d66
Ryan Johnson <rjohnson008@yahoo.com>
2013-02-27 10:52:34 -0800

    Add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,4 +3,9 @@ class Status < ActiveRecord::Base
 
   # A status belongs to a single user
   belongs_to :user
+
+  validates :content, presence: :true,
+                              length: { minimum: 2 }
+
+  validates :user_id, presence: :true
 end
